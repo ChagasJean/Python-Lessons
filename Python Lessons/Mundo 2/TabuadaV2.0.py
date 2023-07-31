908346654ecf5ce5b714c25a7effdce8b4f38123
Jean Morais <jeandaschagas1@gmail.com>
2023-07-31 15:16:19 -0300

    Fiz o exercício de Tabuada V2.0

--- Python Lessons/Mundo 2/TabuadaV2.0.py
+++ Python Lessons/Mundo 2/TabuadaV2.0.py
@@ -1,1 +1,7 @@
 # REFAÇA O DESAFIO DA TABUADA, MOSTRANDO A TABUADA DE UM NÚMERO QUE O USUÁRIO ESCOLHER, SÓ QUE AGORA UTLIZANDO O LAÇO FOR.
+
+n = int(input('Digite um valor: '))
+
+for num in range(1, 11):
+    m = n * num
+    print('{} * {}: {}'.format(n, num, m))
