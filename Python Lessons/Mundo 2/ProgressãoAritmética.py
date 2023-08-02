6b0681cd3e8dcff571aaf322e046a9564c1687ab
Jean Morais <jeandaschagas1@gmail.com>
2023-08-02 02:19:10 -0300

    Fiz o exercicio da Progressão Aritimética

--- Python Lessons/Mundo 2/ProgressãoAritmética.py
+++ Python Lessons/Mundo 2/ProgressãoAritmética.py
@@ -1,1 +1,9 @@
 # DESENVOLVA UM PROGRAMA QUE LEIA O PRIMEIRO TERMO E A RAZÃO DE UMA P.A. NO FINAL, MOSTRE OS 10 PRIMEIROS TERMOS DESSA PROGRESSÃO.
+
+primeiro_termo = int(input("Digite o primeiro termo da P.A.: "))
+razao = int(input("Digite a razão da P.A.: "))
+
+print("Os 10 primeiros termos da P.A. são:")
+for i in range(10):
+    termo = primeiro_termo + i * razao
+    print(termo, end=" ")
